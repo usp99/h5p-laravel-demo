d114d43d27bb7eed23b84e500899f9f375c6be4f
PARMAR <PARMAU02@pfizer.com>
2024-06-25 15:24:16 +0530

    copied js files to public directory

--- resources/views/h5p/create.blade.php
+++ resources/views/h5p/create.blade.php
@@ -2,9 +2,9 @@
 <html>
     <head>
         <title>Create Content</title>
-        <link rel="stylesheet" href="{{ asset('h5p/h5p-core/styles/h5p-confirmation-dialog.css') }}"></link>
+        <link rel="stylesheet" href="{{ asset('h5p/h5p-core/styles/application.css') }}">
         @foreach ($settings['editor']['assets']['css'] as $style)
-            <link rel="stylesheet" href="{{ $style }}">
+            <link rel="stylesheet" href="{{ asset('h5p/h5p-core/'.$style) }}">
         @endforeach
     </head>
 
@@ -13,8 +13,42 @@
         </div>
 
         @foreach ($settings['editor']['assets']['js'] as $script)
-            <script src="{{ $script }}"></script>
+            <script src="{{ asset('h5p/h5p-core/'.$script) }}"></script>
         @endforeach
+
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-editor.js') }}"></script>
+        {{-- <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-av.js') }}"></script> --}}
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-boolean.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-coordinates.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-dimensions.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-file-uploader.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-file.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-form.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-fullscreen-bar.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-fullscreen-bar.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-group.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-html.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-image-popup.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-image.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-init.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-library-list-cache.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-library-selector.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-library.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-list-editor.js') }}"></script>
+        {{-- <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-list.js') }}"></script> --}}
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-metadata-author-widget.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-metadata-changelog-widget.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-metadata.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-none.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-number.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-pre-save.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-select.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-selector-hub.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-selector-legacy.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-semantic-structure.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-text.js') }}"></script>
+        <script src="{{ asset('h5p/h5p-editor/scripts/h5peditor-textarea.js') }}"></script>
 
         <script>
             H5PEditor.init({
